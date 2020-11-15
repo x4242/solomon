3a6930d29e26810e505ffa20915282e6782b237f
0x4242 <0x4242@protonmail.com>
2020-11-15 18:45:14 +0100

    bug fixes

--- unbound/entrypoint.sh
+++ unbound/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-
 set -e
-
-printf "Running Alpine upgrade.\n"
-apk update
-apk upgrade
-rm -rf /var/cache/apk/*
-printf "Alpine upgrade done.\n"
-
-crond
-
+crond -L /dev/stdout
 exec "$@"
--- unbound/update.sh
+++ unbound/update.sh
@@ -13,9 +13,17 @@
 #   - 2020-01-25: changed to #!/bin/sh for compatibility
 #   - 2020-01-12: created
 
+
+printf "Running Alpine upgrade.\n"
+apk update
+apk upgrade
+rm -rf /var/cache/apk/*
+printf "Alpine upgrade done.\n"
+
 wget https://www.internic.net/domain/named.root -O /etc/unbound/root.hints
 unbound-anchor -v
-python3 generate_blacklist.py
+
+generate_blacklist.py
 unbound-checkconf /tmp/blacklist.conf.tmp
 if [ $? -eq 0 ]; then
   cat /tmp/blacklist.conf.tmp > /etc/unbound/blacklist.conf
