2018065d7363574635b774ebe7df6269ed820d5a
0x4242 <0x4242@protonmail.com>
2020-11-16 19:45:32 +0100

    kea auto update test

--- unbound/update.sh
+++ unbound/update.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-#
-# ------------------------------------------
-# Description
-# ------------------------------------------
-# tbd
-#
-# ------------------------------------------
-# Change History
-# ------------------------------------------
-# lastmod: 2020-01-25T18:28:06+01:00
-# changelog:
-#   - 2020-01-25: changed to #!/bin/sh for compatibility
-#   - 2020-01-12: created
 
 printf "Running Alpine upgrade.\n"
 apk update
